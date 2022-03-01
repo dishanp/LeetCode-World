016e7b8b6585bf258d4329c6a7871178d2d735d3
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2022-03-01 18:10:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.75%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,13 +3,15 @@ public:
     double myPow(double x, int n) {
         if(n==0)
             return 1;
-        double temp=myPow(x,n/2);
-        if(n%2==0){
-            return temp*temp;
-        }else{
-            if(n<0)
-                return 1/x*temp*temp;
-            return temp*temp*x;
+        double res=myPow(x,n/2);
+        if(n%2==0)
+            return res*res;
+        else
+        {
+            if(n>0)
+                return res*res*x;
+            return res*res*1/x;
         }
+        return 0;
     }
 };
